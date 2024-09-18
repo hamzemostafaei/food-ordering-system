c70230504563c9d023b89409e7f38413118bdf86
hamze <hamze.mostafaei@gmail.com>
2024-09-19 00:52:05 +0330

    Refactoring Order domain layer Adding Outbox cleaner scheduler for Payment

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/oubox/scheduler/payment/PaymentOutboxHelper.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/oubox/scheduler/payment/PaymentOutboxHelper.java
@@ -1,7 +1,10 @@
 package com.food.ordering.system.order.service.domain.oubox.scheduler.payment;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.food.ordering.system.domain.value.object.OrderStatus;
 import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import com.food.ordering.system.order.service.domain.oubox.model.payment.OrderPaymentEventPayload;
 import com.food.ordering.system.order.service.domain.oubox.model.payment.OrderPaymentOutboxMessage;
 import com.food.ordering.system.order.service.domain.ports.output.repository.IPaymentOutboxRepository;
 import com.food.ordering.system.outbox.OutboxStatus;
@@ -13,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static com.food.ordering.system.saga.order.SagaConstants.ORDER_SAGA_NAME;
 
@@ -49,6 +53,40 @@ public class PaymentOutboxHelper {
                     orderPaymentOutboxMessage.getId());
         }
         log.info("OrderPaymentOutboxMessage saved with outbox id: {}", orderPaymentOutboxMessage.getId());
+    }
+
+    @Transactional
+    public void savePaymentOutboxMessage(OrderPaymentEventPayload paymentEventPayload,
+                                         OrderStatus orderStatus,
+                                         SagaStatus sagaStatus,
+                                         OutboxStatus outboxStatus,
+                                         String sagaId) {
+        save(OrderPaymentOutboxMessage.builder()
+                .id(UUID.randomUUID().toString())
+                .sagaId(sagaId)
+                .createdAt(paymentEventPayload.getCreatedAt())
+                .type(ORDER_SAGA_NAME)
+                .payload(createPayload(paymentEventPayload))
+                .orderStatus(orderStatus)
+                .sagaStatus(sagaStatus)
+                .outboxStatus(outboxStatus)
+                .build());
+    }
+
+    @Transactional
+    public void deletePaymentOutboxMessageByOutboxStatusAndSagaStatus(OutboxStatus outboxStatus,
+                                                                      SagaStatus... sagaStatus) {
+        paymentOutboxRepository.deleteByTypeAndOutboxStatusAndSagaStatus(ORDER_SAGA_NAME, outboxStatus, sagaStatus);
+    }
+
+    private String createPayload(OrderPaymentEventPayload paymentEventPayload) {
+        try {
+            return objectMapper.writeValueAsString(paymentEventPayload);
+        } catch (JsonProcessingException e) {
+            log.error("Could not create OrderPaymentEventPayload object for order id: {}",paymentEventPayload.getOrderId(), e);
+            throw new OrderDomainException(
+                    String.format("Could not create OrderPaymentEventPayload object for order id: [%s]", paymentEventPayload.getOrderId()));
+        }
     }
 
 }
