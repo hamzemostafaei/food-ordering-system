8c62d516dc2dc1756585866ce2af16c44451adc4
hamze <hamze.mostafaei@gmail.com>
2024-10-21 16:02:11 +0330

    Customer service entity scan and repository package path bug fix

--- customer-service/customer-container/src/main/java/com/food/ordering/system/customer/service/CustomerServiceApplication.java
+++ customer-service/customer-container/src/main/java/com/food/ordering/system/customer/service/CustomerServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
-@EntityScan(basePackages = {"com.food.ordering.system.customer.service.data-access", "com.food.ordering.system.data-access"})
-@EnableJpaRepositories(basePackages = {"com.food.ordering.system.customer.service.data-access", "com.food.ordering.system.data-access"})
+@EntityScan(basePackages = {"com.food.ordering.system.customer.service.data.access", "com.food.ordering.system.data.access"})
+@EnableJpaRepositories(basePackages = {"com.food.ordering.system.customer.service.data.access", "com.food.ordering.system.data.access"})
 public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
