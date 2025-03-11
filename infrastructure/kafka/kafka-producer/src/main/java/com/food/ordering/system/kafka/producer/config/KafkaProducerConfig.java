1f3c8cc2f469272d76fa4b11a5f5547b35378b87
hamze <hamze.mostafaei@gmail.com>
2025-03-11 13:59:25 +0330

    Trivial change

--- infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/config/KafkaProducerConfig.java
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/config/KafkaProducerConfig.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.kafka.producer;
+package com.food.ordering.system.kafka.producer.config;
 
 import com.food.ordering.system.kafka.config.data.KafkaConfigData;
 import com.food.ordering.system.kafka.config.data.KafkaProducerConfigData;
